8606b577dff45515102eb04bfcc07805404264e2
GaryFishback <gary.joel.fishback@gmail.com>
2020-06-04 01:35:46 -0500

    learning Mongodb

---
+++ src/models/models.js
@@ -0,0 +1,22 @@
+import mongoose from ('mongoose')
+import { Schema } from "mongoose"
+
+var newItem = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+})
